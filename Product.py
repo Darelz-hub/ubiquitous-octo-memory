cc834dbf850caa4d9875241e48478c5ec658c5f0
Wadim <Qazxcv33@yandex.ru>
2023-10-08 13:26:17 +0500

    Function

--- Product.py
+++ Product.py
@@ -12,21 +12,102 @@ c.execute('''
         price INTEGER NOT NULL
     )
 ''')
-# Добавление товаров в базу данных
-c.execute("INSERT INTO products (name, quantity, price) VALUES ('лате', 200, 200)")
-c.execute("INSERT INTO products (name, quantity, price) VALUES ('капучино', 250, 250)")
-c.execute("INSERT INTO products (name, quantity, price) VALUES ('круасан', 100, 100)")
-c.execute("INSERT INTO products (name, quantity, price) VALUES ('пончик', 150, 150)")
+# # Добавление товаров в базу данных
+# c.execute("INSERT INTO products (name, quantity, price) VALUES ('лате', 200, 200)")
+# c.execute("INSERT INTO products (name, quantity, price) VALUES ('капучино', 250, 250)")
+# c.execute("INSERT INTO products (name, quantity, price) VALUES ('круасан', 100, 100)")
+# c.execute("INSERT INTO products (name, quantity, price) VALUES ('пончик', 150, 150)")
 
-# Добавление нового товара
-c.execute("INSERT INTO products (name, quantity, price) VALUES ('эспрессо', 180, 180)")
+# # Добавление нового товара
+# c.execute("INSERT INTO products (name, quantity, price) VALUES ('эспрессо', 180, 180)")
 
-# Удаление товара по id
-product_id = 1
-c.execute("DELETE FROM products WHERE id = ?", (product_id,))
+# # Удаление товара по id
+# product_id = 1
+# c.execute("DELETE FROM products WHERE id = ?", (product_id,))
 
-# Редактирование имя и количество товара по id
-product_id = 2
-new_name = "Капучино XL"
-new_quantity = 300
-c.execute("UPDATE products SET name = ?, quantity = ? WHERE id = ?", (new_name, new_quantity, product_id))
+# # Редактирование имя и количество товара по id
+# product_id = 2
+# new_name = "Капучино XL"
+# new_quantity = 300
+# c.execute("UPDATE products SET name = ?, quantity = ? WHERE id = ?", (new_name, new_quantity, product_id))
+
+# Выбор функции
+def choice_f():
+    while True:
+        print("\nФункции:")
+        print("1. Добавить товар")
+        print("2. Редактировать товар")
+        print("3. Удалить товар")
+        print("4. Просмотреть все товары")
+        print("5. Выйти")
+
+        choice = input("Выберите номер функции: ")
+
+        if choice == '1':
+            add_product()
+        elif choice == '2':
+            edit_product()
+        elif choice == '3':
+            delete_product()
+        elif choice == '4':
+            view_products()
+        elif choice == '5':
+            break
+        else:
+            print("Неверный выбор функции")
+
+# Добавление товара
+def add_product():
+    name = input("Введите наименование товара: ")
+    quantity = int(input("Введите количество товара: "))
+    price = float(input("Введите стоимость одной единицы товара: "))
+    conn = sqlite3.connect('coffeeshop.db')
+    c = conn.cursor()
+    c.execute('''
+        INSERT INTO products (name, quantity, price)
+        VALUES (?, ?, ?)
+    ''', (name, quantity, price))
+    print("Товар успешно добавлен!")
+# Редактирование товаров
+def edit_product():
+    product_id = int(input("Введите id товара для редактирования: "))
+    name = input("Введите новое наименование товара: ")
+    quantity = int(input("Введите новое количество товара: "))
+    price = float(input("Введите новую стоимость одной единицы товара: "))
+
+    conn = sqlite3.connect('coffeeshop.db')
+    c = conn.cursor()
+    c.execute('''
+        UPDATE products
+        SET name = ?, quantity = ?, price = ?
+        WHERE id = ?
+    ''', (name, quantity, price, product_id))
+    print("Товар успешно отредактирован!")
+
+# Удаление товаров
+def delete_product():
+    product_id = int(input("Введите id товара для удаления: "))
+    conn = sqlite3.connect('coffeeshop.db')
+    c = conn.cursor()
+    c.execute('''
+        DELETE FROM products
+        WHERE id = ?
+    ''', (product_id,))
+    print("Товар удален!")
+
+# Просмотр товаров
+def view_products():
+    conn = sqlite3.connect('coffeeshop.db')
+    c = conn.cursor()
+    c.execute('''
+        SELECT * FROM products
+    ''')
+    products = c.fetchall()
+    conn.close()
+
+    print("Список товаров:")
+    print("{:<10} {:<30} {:>10} {:>10}".format("ID", "Наименование", "Количество", "Цена"))
+    print("-" * 60)
+    for product in products:
+        print("{:<10} {:<30} {:>10} {:>10.2f}".format(product[0], product[1], product[2], product[3]))
+
