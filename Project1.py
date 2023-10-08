e0b1cfacd142ed6bbc3959a500302ac08048f860
Maksim <m.boltobogov@gmail.com>
2023-10-08 10:35:00 +0300

    create branch Maksim

--- Project1.py
+++ Project1.py
@@ -51,7 +51,7 @@ with sq.connect('coffee_house.db') as con:
                     
                     CREATE TABLE IF NOT EXISTS products (
                         id INTEGER PRIMARY KEY AUTOINCREMENT ,
-                        name TEXT NOT NULL,
+                        name TEXT NOT NULL Unique,
                         quantity INTEGER NOT NULL,
                         price INTEGER NOT NULL
                     );
