d7a5e4fa96979c6b90022ceea7025a9ed6c5e10b
DimOn <dim@on>
2023-10-14 11:15:34 +0300

    corrections are added

--- git.py
+++ git.py
@@ -15,72 +15,71 @@ with sqlite3.connect("coffee.db") as con:
         );
     """)
 
-    def menu():
-        while True:
-            choose_main = input("""\nМеню смены:
-    1 - добавить
-    2 - просмотр
-    3 - редактировать
-    4 - отмена
+def menu():
+    while True:
+        choose_main = input("""\nМеню смены:
+1 - добавить
+2 - просмотр
+3 - редактировать
+4 - отмена
 >> """)
 
-            #отмена/выход в главное меню
-            if choose_main == '4':
-                break
+        #отмена/выход в главное меню
+        if choose_main == '4':
+            break
 
-            # добавить
-            elif choose_main == '1':
-                add_shit()
+        # добавить
+        elif choose_main == '1':
+            add_shit()
 
-            # посмотреть
-            elif choose_main == '2':
-                show_shift()
+        # посмотреть
+        elif choose_main == '2':
+            show_shift()
 
-            # изменить
-            elif choose_main == '3':
-                change_shift()
+        # изменить
+        elif choose_main == '3':
+            change_shift()
 
-    def add_shit():
-        shift = input('смена (дневная/вечерняя): ')
-        date_year, date_month, date_day = input('год: '), input(' месяц: '), input('  день: ')
-        revenue = int(input('выручка: '))
-        id_e = int(input('id сотрудника: '))
-        with sqlite3.connect("coffee.db") as con:
-            cur = con.cursor()
-            cur.execute(f"""
-                        INSERT INTO work_shift (shift, date_year, date_month, date_day, revenue, id_e) VALUES (
-                        '{shift}', {date_year}, {date_month}, {date_day}, '{revenue}', '{id_e}'
-                        );
-                        """)
+def add_shit():
+    shift = input('смена (дневная/вечерняя): ')
+    date_year, date_month, date_day = input('год: '), input(' месяц: '), input('  день: ')
+    revenue = int(input('выручка: '))
+    id_e = int(input('id сотрудника: '))
+    with sqlite3.connect("coffee.db") as con:
+        cur = con.cursor()
+        cur.execute(f"""
+                    INSERT INTO work_shift (shift, date_year, date_month, date_day, revenue, id_e) VALUES (
+                    '{shift}', {date_year}, {date_month}, {date_day}, '{revenue}', '{id_e}'
+                    );
+                    """)
 
-    def show_shift():
+def show_shift():
+    result = cur.execute("SELECT * FROM work_shift;")
+    for row in result.fetchall():
+        print(f"{row[0]}) {row[1]} ({row[2]}-{row[3]}-{row[4]}), $: {row[5]}, id: {row[6]}")
+
+def change_shift():
+    with sqlite3.connect("coffee.db") as con:
+        cur = con.cursor()
         result = cur.execute("SELECT * FROM work_shift;")
         for row in result.fetchall():
-            print(f"{row[0]}) {row[1]} ({row[2]}-{row[3]}-{row[4]}), $: {row[5]}, id: {row[6]}")
-
-    def change_shift():
-        with sqlite3.connect("coffee.db") as con:
-            cur = con.cursor()
-            result = cur.execute("SELECT * FROM work_shift;")
-            for row in result.fetchall():
-                print(row)
-            choose_3 = input('''    Изменить:
-        1 - выручку за смену
-        2 - сотрудника на смене
+            print(row)
+        choose_3 = input('''    Изменить:
+    1 - выручку за смену
+    2 - сотрудника на смене
     >> ''')
-            if choose_3 == '1':
-                ids = int(input('какая смена (id): '))
-                new_revenue = int(input('ввести выручку: '))
-                cur.execute(f"""
-                            UPDATE work_shift SET revenue='{new_revenue}' WHERE id={ids};
-                            """)
-            elif choose_3 == '2':
-                ids = int(input('какая смена (id): '))
-                new_id_e = int(input('заменить на сотрудника: '))
-                cur.execute(f"""
-                            UPDATE work_shift SET id_e='{new_id_e}' WHERE id={ids};
-                            """)
+        if choose_3 == '1':
+            ids = int(input('какая смена (id): '))
+            new_revenue = int(input('ввести выручку: '))
+            cur.execute(f"""
+                        UPDATE work_shift SET revenue='{new_revenue}' WHERE id={ids};
+                        """)
+        elif choose_3 == '2':
+            ids = int(input('какая смена (id): '))
+            new_id_e = int(input('заменить на сотрудника: '))
+            cur.execute(f"""
+                        UPDATE work_shift SET id_e='{new_id_e}' WHERE id={ids};
+                        """)
 
 
 menu()
-
